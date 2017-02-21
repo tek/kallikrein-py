c5a45f1bf1be229babef24bca112c65b4b32bf47
Torsten Schmits <torstenschmits@gmail.com>
2017-02-21 22:09:34 +0100

    add cwd to path when running cli

--- kallikrein/run/cli.py
+++ kallikrein/run/cli.py
@@ -1,3 +1,6 @@
+import sys
+import os
+
 from golgi import Config, cli
 
 from amino import _
@@ -7,6 +10,7 @@ from kallikrein.run.main import kallikrein_run_lazy
 
 @cli(positional=(('specs', '*'),))
 def klk() -> int:
+    sys.path.insert(0, os.getcwd())
     specs = Config['run'].specs
     return 0 if kallikrein_run_lazy(specs).exists(_.success) else 1
 
